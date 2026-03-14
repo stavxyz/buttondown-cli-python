86616a75f2c453fddbcd3e5a5cfeabf4aea16728
samuel <hi@stav.xyz>
2026-03-14 15:39:36 -0500

    Support -h as alias for --help on all commands

--- src/bd/cli.py
+++ src/bd/cli.py
@@ -15,6 +15,7 @@ app = typer.Typer(
     name="bd",
     help="Buttondown newsletter CLI.",
     no_args_is_help=True,
+    context_settings={"help_option_names": ["-h", "--help"]},
 )
 console = Console()
 
